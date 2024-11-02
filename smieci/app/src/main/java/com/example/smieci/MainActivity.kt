17420ee2eedc32c7032cec54755c77fdc008d47d
MajaZa <zadwornamaja87@gmail.com>
2024-11-02 15:58:09 +0100

    world

--- smieci/app/src/main/java/com/example/smieci/MainActivity.kt
+++ smieci/app/src/main/java/com/example/smieci/MainActivity.kt
@@ -16,32 +16,6 @@ import com.example.smieci.ui.theme.SmieciTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            SmieciTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmieciTheme {
-        Greeting("Android")
-    }
-}
