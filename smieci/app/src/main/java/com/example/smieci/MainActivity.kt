015c7f68a7833d693f684f34cc9e69b7549eca26
Jevmon <szymmiel10@gmail.com>
2024-11-03 21:02:26 +0100

    Main cd

--- smieci/app/src/main/java/com/example/smieci/MainActivity.kt
+++ smieci/app/src/main/java/com/example/smieci/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.smieci
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.NonNull
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.text.util.LocalePreferences.CalendarType
+import com.example.smieci.ui.theme.SmieciTheme
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.logowanie)
+        enableEdgeToEdge()
+        setContentView(R.layout.main)
 
 
     }
+
+
 }
