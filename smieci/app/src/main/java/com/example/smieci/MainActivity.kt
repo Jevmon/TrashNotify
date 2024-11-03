6714975ace4dee5adf90c0eed6875469ee04d61b
MajaZa <zadwornamaja87@gmail.com>
2024-11-03 03:51:23 +0100

    layout logowanie i rejestracja

--- smieci/app/src/main/java/com/example/smieci/MainActivity.kt
+++ smieci/app/src/main/java/com/example/smieci/MainActivity.kt
@@ -1,26 +1,14 @@
 package com.example.smieci
 
 import android.os.Bundle
-import android.view.Menu
-import android.widget.CalendarView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.smieci.ui.theme.SmieciTheme
 
 class MainActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
+        setContentView(R.layout.logowanie)
 
 
     }
